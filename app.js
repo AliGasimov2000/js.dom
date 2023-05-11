732b322546d51d9683491b058c6fce8d00b3ffdb
Ali Gasimov <gasimovali009@gmail.com>
2023-05-11 05:19:55 +0000

    message

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 "use strict";
 $(document).ready(function () {
-   
-   
-   
-   
-   
-   
-   
+
+
+
+
+
+
+
     $("#me").click(function (e) {
         e.preventDefault();
         function myel(myclass = "") {
@@ -27,6 +27,10 @@ $(document).ready(function () {
         $("#messageyou").append(myel())
         $("#messageme").append(myel('class= "end"'))
         $("#inputme").val("")
+
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+
     });
 
     $("#you").click(function (e) {
@@ -50,15 +54,15 @@ $(document).ready(function () {
         $("#messageyou").append(myel('class= "end"'))
         $("#inputyou").val("")
 
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)     
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
         document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
 
     });
 
 
-   
 
 
 
-   
+
+
 });
